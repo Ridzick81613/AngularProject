1e9c4dd5578ef1a15d16d3290886d6fd984167da
Ridzick81613 <MRytz55@gmail.com>
2016-11-10 18:11:59 -0600

    Got a good start (finally) on the route to get the weather from openweathermap api

--- public/assets/scripts/app.js
+++ public/assets/scripts/app.js
@@ -1,4 +1,28 @@
-var app = angular.module("RedEye", [])
+var app = angular.module("RedEye", []);
+
+app.controller("weatherCtrl", MyCtrl)
+  function MyCtrl($scope, $http) {
+    var usersZip = $scope.zip;
+
+    $scope.findWeather = function(){
+      $http({
+      method: 'POST',
+      data: {"zipCode": usersZip},
+      url: '/getWeather'
+    }).then(function successCallback(response) {
+      console.log(response.body);
+     $scope.printarea = response.body;
+      }, function errorCallback(response) {
+        console.log("An error occurred.")
+      });
+
+    }
+  }
+
+//http://api.openweathermap.org/data/2.5/weather?zip=' + $scope.zipcode + ',us&appid=eee7a2ab44a813468869618fa17f7c8e
+
+
+
 
 // app.controller('weatherCtrl', function($scope, $http)
 // {
@@ -27,22 +51,3 @@ var app = angular.module("RedEye", [])
 // });
 
 //controller code for weatherCtrl found on stackoverflow link:http://stackoverflow.com/questions/35236819/angularjs-using-http-get-with-ng-model authors: charlietfl & Eric Scandora
-
-app.controller("weatherCtrl", MyCtrl);
-
-
-function MyCtrl($scope, $http) {
-
-    $scope.location = "";
-
-    $scope.findWeather = function(){
-    $http.get('/getWeather').success(function(data){
-    $scope.weatherLocation = data.name;
-    $scope.windSpeed = data.wind.speed;
-    $scope.windDegree = data.wind.deg;
-
-    })
-    }
-}
-
-//http://api.openweathermap.org/data/2.5/weather?zip=' + $scope.zipcode + ',us&appid=eee7a2ab44a813468869618fa17f7c8e
--- server.js
+++ server.js
@@ -1,25 +1,25 @@
 var express = require('express')
 var app = express()
 var request = require('request');
+var bodyParser = require('body-parser')
 
 
 app.use(express.static('public'));
 
-app.get('/getWeather', function (req, res) {
-
-    request('http://api.openweathermap.org/data/2.5/forecast?lat=35&lon=139&appid=eee7a2ab44a813468869618fa17f7c8e', function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-        console.log(body);
-      } else {
-        console.warn(error);
-      }
+app.post('/getWeather', function (req, res) {
+  console.log('hello');
+  var zipcode = req.body.data.zipCode;
+  console.log('hello');
+  request('http://api.openweathermap.org/data/2.5/weather?zip=' + zipcode + ',us&appid=eee7a2ab44a813468869618fa17f7c8e', function (error, response, body)
+  {
+    if (!error && response.statusCode == 200) {
+      console.log(body);
+    } else {
+      console.warn(error);
+    }
+    });
 });
-app.post('/getWeather', function(req, res) {
-    var data = req.body
-
-    res.send(data);
-});})
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
-})
+});
