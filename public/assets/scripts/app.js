a4e337a6c8eacb4ff4e5c4bfe8fb2850bf38cf77
Ridzick81613 <MRytz55@gmail.com>
2016-11-14 00:10:13 -0600

    Finally got the server to pull weather from API and send it back to client side. Displays on the page. Need to pretty-print.

--- public/assets/scripts/app.js
+++ public/assets/scripts/app.js
@@ -2,52 +2,15 @@ var app = angular.module("RedEye", []);
 
 app.controller("weatherCtrl", MyCtrl)
   function MyCtrl($scope, $http) {
-    var usersZip = $scope.zip;
+    // var usersZip = $scope.zip;
 
     $scope.findWeather = function(){
-      $http({
-      method: 'POST',
-      data: {"zipCode": usersZip},
-      url: '/getWeather'
-    }).then(function successCallback(response) {
-      console.log(response.body);
-     $scope.printarea = response.body;
+      $http.post('/getWeather', $scope.zip).then(function successCallback(response) {
+      console.log(response.data);
+     $scope.printarea = response.data;
       }, function errorCallback(response) {
-        console.log("An error occurred.")
+        $scope.printarea="An error occurred.";
       });
 
     }
   }
-
-//http://api.openweathermap.org/data/2.5/weather?zip=' + $scope.zipcode + ',us&appid=eee7a2ab44a813468869618fa17f7c8e
-
-
-
-
-// app.controller('weatherCtrl', function($scope, $http)
-// {
-//   $scope.getData = function()
-//   {
-//     $http.get('http://api.openweathermap.org/data/2.5/weather?zip=' + '$scope.zipcode' + ',us&appid=eee7a2ab44a813468869618fa17f7c8e')
-//         .then(function(response)
-//         {
-//           $scope.myData = response;
-//           console.log($scope.myData);
-//         },
-//
-//       function(response)
-//       {
-//           $scope.myData = "Something went wrong";
-//       });
-//
-//   app.filter('kelvinToFahrenheit', function()
-//   {
-//     return function(kelvin)
-//     {
-//       return (parseFloat(kelvin) - 273.15) * 1.80 + 32.00;
-//     }
-//   });
-// };
-// });
-
-//controller code for weatherCtrl found on stackoverflow link:http://stackoverflow.com/questions/35236819/angularjs-using-http-get-with-ng-model authors: charlietfl & Eric Scandora
--- server.js
+++ server.js
@@ -7,13 +7,13 @@ var bodyParser = require('body-parser')
 app.use(express.static('public'));
 
 app.post('/getWeather', function (req, res) {
-  console.log('hello');
-  var zipcode = req.body.data.zipCode;
-  console.log('hello');
-  request('http://api.openweathermap.org/data/2.5/weather?zip=' + zipcode + ',us&appid=eee7a2ab44a813468869618fa17f7c8e', function (error, response, body)
+  var zipcode = req.body;
+  console.log(zipcode);
+  request('http://api.openweathermap.org/data/2.5/weather?zip=' + zipcode + ',us&units=imperial&appid=eee7a2ab44a813468869618fa17f7c8e', function (error, response, body)
   {
     if (!error && response.statusCode == 200) {
       console.log(body);
+      res.send(body);
     } else {
       console.warn(error);
     }
